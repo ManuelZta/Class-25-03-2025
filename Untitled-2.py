29e47249ad2189f827460361392005faee58c70e
ManuelZta <manuel.zapata1@udea.edu.co>
2025-03-25 17:48:10 -0500

    ejemplo 1 Clase de 25/03/2025

---
+++ Untitled-2.py
@@ -0,0 +1,9 @@
+fact1 = int(input("#1: ")) #2
+fact2 = int(input("#2: ")) #3
+c = 0
+resultado = 0
+while c < fact1:
+    resultado += fact2
+    print(resultado)
+    c += 1
+print(f"Resultado Final: {resultado}")
